d97d601b0d16bc335284cea8023b244b2b6c3146
juan <jcortesj@unal.edu.co>
2022-10-28 00:30:39 -0500

    probemos...

--- software/frontend/landing-page/my-app/pages/dashBoard.tsx
+++ software/frontend/landing-page/my-app/pages/dashBoard.tsx
@@ -30,7 +30,7 @@ const DashBoard = () => {
     useEffect(
         ()=>{
             async function getPageData(){
-                const apiUrlEndpoint = './api/getData';
+                const apiUrlEndpoint = '/api/getData';
                 const response = await fetch(apiUrlEndpoint)
                 const res = await response.json();
                 console.log(res);
